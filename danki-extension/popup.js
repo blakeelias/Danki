24423f63d0770ae0409ba03a36d9ee99db193f5f
Blake Elias <blakeelias@gmail.com>
2017-02-04 21:03:54 -0500

    Remove un-necessary code from example Chrome extension.

--- danki-extension/popup.js
+++ danki-extension/popup.js
@@ -47,71 +47,6 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
-
-function getPageText(url, callback, errorCallback) {
-    var x = new XMLHttpRequest();
-    x.open('GET', url);
-    // The Google image search API responds with JSON, so let Chrome parse it.                      
-    x.responseType = 'text';
-    x.onload = function() {
-	// Parse and process the response from Google Image Search.                                   
-	var response = x.response;
-	if (!response) {
-	    errorCallback('No response from \n' + url);
-	    return;
-	}
-	callback(response);
-    };
-    x.onerror = function() {
-	errorCallback('Network error.');
-    };
-    x.send();
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
 function drawRow(rowData) {
 	var row = $("<tr />")
 	$("#flashcardTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
@@ -132,26 +67,4 @@ document.addEventListener('DOMContentLoaded', function() {
 			  c.lastChild.childNodes[1].textContent = l[i].back;
 			  }
   });
-  /*getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = 'http://animaliaz-life.com/data_images/dog/dog6.jpg' //imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });*/
 });
